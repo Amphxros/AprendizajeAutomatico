0454fc3c553572fe74ba41c5619db14e462cdeca
A. Rubio <ampxrubio@gmail.com>
2023-09-21 16:49:47 +0100

    Cosas

---
+++ Practicas/Practica0/Practica0-Montecarlo.py
@@ -0,0 +1,18 @@
+# Practica 0 ejercicio de Montecarlo
+
+# Montecarlo: calculo de pi con puntos aleatorios
+
+import pandas as pd
+import numpy as np
+
+#Point: definicion de puntos en el espacio bidimensional
+class Point:
+    def __init__(self,x,y): 
+        self.x=x
+        self.y=y
+
+class Montecarlo:
+    def __init__(self,numX,numY):
+        X=np.random.random((numX,numY))
+        print(X) 
+        
