748baf805139c6ccd6bda5dcc144c1faf4601461
A. Rubio <ampxrubio@gmail.com>
2023-10-18 11:36:18 +0100

    Linear regresion thingy

--- Practicas/Practica1/Practica/linear_reg.py
+++ Practicas/Practica1/Practica/linear_reg.py
@@ -2,9 +2,6 @@ import numpy as np
 import copy
 import math
 
-
-
-
 #########################################################################
 # Cost function
  """Computes the cost function for linear regression.
@@ -19,13 +16,16 @@ import math
                to fit the data points in x and y
 """
 def compute_cost(x, y, w, b):
-    total_cost=0
-    for i in range(len(x)):
-        J=((w + b*x[i]) - y[i]**i)**2
-        total_cost= total_cost + J
-    
-    
-    total_cost= (1/2*len(x))*total_cost
+     m = len(y)  # Nº examples
+
+    # Calcular la hipótesis
+    h = w * x + b # linear regresion so f(x)= w*x + b
+
+    #  (MSE - Mean Squared Error)
+    error = h-y
+    squared_error = error**2
+    total_cost = (1 / (2*m)) * np.sum(squared_error)
+
     return total_cost
 
 
@@ -43,6 +43,18 @@ def compute_cost(x, y, w, b):
      """
 #
 def compute_gradient(x, y, w, b):
+    
+    m = len(y) # Nº examples
+    
+    # hipotesis
+    h = w*x + b # linear regresion so f(x)= w*x + b
+
+    # gradient w (dj_dw)
+    dj_dw = (1/m) * np.sum((h-y) * x)
+
+    # gradient b (dj_db)
+    dj_db = (1/m) * np.sum(h-y)
+
     
     return dj_dw, dj_db
 
@@ -71,13 +83,18 @@ def compute_gradient(x, y, w, b):
 
 #
 def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
-    theta=0
-    w=0
-    b=0
+    J_history=np.zeros(num_iters)
+    
+    w=w_in
+    b=_b_in
+    
+    m=len(y) # Nº examples
+    
     for i in range(num_iters):
-        w=cost_function(x,y,theta)
-        aux=gradient_function(x,y,theta,alpha)
-        theta=aux
+        dj_dw, dj_db=  compute_gradient(x,y,w,b)
+        w-=alpha * dj_dw
+        b-=alpha * dj_db
+        J_history[i]= compute_cost(x,y,w,b)
         
     
     return w, b, J_history
