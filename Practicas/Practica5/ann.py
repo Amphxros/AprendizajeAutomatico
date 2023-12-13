ae4be9b2b4bd3fd1dc663881100bc75862e57019
A. Rubio <ampxrubio@gmail.com>
2023-12-13 23:47:02 +0100

    cosas

--- Practicas/Practica5/ann.py
+++ Practicas/Practica5/ann.py
@@ -1,5 +1,9 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
+
+from scipy.optimize import minimize
+
 
 def initialize_params(input_size, hidden_size, output_size):
     epsilon_init = 0.12
@@ -10,7 +14,8 @@ def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
 def cost(theta1, theta2, X, y, lambda_):
-   """Compute cost for 2-layer neural network. Parameters
+   """
+   Compute cost for 2-layer neural network. Parameters
    ----------
     theta1 : array_like
     Weights for the first layer in the neural network.
@@ -31,25 +36,22 @@ def cost(theta1, theta2, X, y, lambda_):
     J : float
     The computed value for the cost function.
     """
-    m = len(y)
-    ones = np.ones((m, 1))
-    a1 = np.hstack((ones, X))
-    z2 = np.dot(a1, theta1.T)
-    a2 = np.hstack((ones, sigmoid(z2)))
-    z3 = np.dot(a2, theta2.T)
-    h = sigmoid(z3)
-              
-    ## coste
-    J=(-1/m)* np.sum(y*np.log(h) + (1 - y) * np.log(1-h))
-    ## regularizacion
-    reg= (lambda_/(2*m))* np.sum(theta1[:,1:]**2) + np.sum(theta2[:,1:]) 
-    J=J+reg
-    return J
+   m = len(y)
+   ones = np.ones((m, 1))
+   a1 = np.hstack((ones, X))
+   z2 = np.dot(a1, theta1.T)
+   a2 = np.hstack((ones, sigmoid(z2)))
+   z3 = np.dot(a2, theta2.T)
+   h = sigmoid(z3)
+   J=(-1/m)* np.sum(y*np.log(h) + (1 - y) * np.log(1-h))
+   reg= (lambda_/(2*m))* np.sum(theta1[:,1:]**2) + np.sum(theta2[:,1:]) 
+   J=J+reg
+   return J
 
 
-def backdrop(theta1. theta2, X,y,lambda_):
+def backdrop(theta1, theta2, X,y,lambda_):
     m=len(y)
-    one=np.ones(m,1)
+    ones=np.ones(m,1)
     a1 = np.hstack((ones, X))
     z2 = np.dot(a1, theta1.T)
     a2 = np.hstack((ones, sigmoid(z2)))
@@ -70,25 +72,28 @@ def backdrop(theta1. theta2, X,y,lambda_):
     
     
     return J, grad1, grad2
-def train_neural_network(X,y,input_size,hidden,output_size,lambda_,alpha,num_iterations):
-    m=X.shape()
-    one=np.ones(m,1)
+def train_neural_network(X,y,input_size,hidden_size,output_size,lambda_,alpha,num_iterations):
+    m=len(X)
     # One-Hot Encoding
-    y_onehot = np.eye(output_size)[y.flatten() - 1]
+    # Inicializar el codificador
+    encoder = OneHotEncoder(sparse=False)
+
+    # Reshape y aplicar one-hot encoding
+    y_onehot = encoder.fit_transform(y.reshape(-1, 1))
     # Initialize parameters
     theta1, theta2 = initialize_params(input_size, hidden_size, output_size)
     for i in range(num_iterations):
          # Flatten parameters for optimization
         params = np.concatenate((theta1.flatten(), theta2.flatten()))
          # Define cost and gradient function for optimization
-        cost_func = lambda p: backprop(np.reshape(p[:hidden_size * (input_size + 1)], (hidden_size, input_size + 1)),
+        cost_func = lambda p: backdrop(np.reshape(p[:hidden_size * (input_size + 1)], (hidden_size, input_size + 1)),
                                        np.reshape(p[hidden_size * (input_size + 1):], (output_size, hidden_size + 1)),
                                        X, y_onehot, lambda_)[0]
 
-        grad_func = lambda p: np.concatenate((backprop(np.reshape(p[:hidden_size * (input_size + 1)], (hidden_size, input_size + 1)),
+        grad_func = lambda p: np.concatenate((backdrop(np.reshape(p[:hidden_size * (input_size + 1)], (hidden_size, input_size + 1)),
                                                        np.reshape(p[hidden_size * (input_size + 1):], (output_size, hidden_size + 1)),
                                                        X, y_onehot, lambda_)[1].flatten(),
-                                              backprop(np.reshape(p[:hidden_size * (input_size + 1)], (hidden_size, input_size + 1)),
+                                              backdrop(np.reshape(p[:hidden_size * (input_size + 1)], (hidden_size, input_size + 1)),
                                                        np.reshape(p[hidden_size * (input_size + 1):], (output_size, hidden_size + 1)),
                                                        X, y_onehot, lambda_)[2].flatten()))
     
