3cdec085dd2c61a54ce971f752fcb9380512cf4a
A. Rubio <ampxrubio@gmail.com>
2024-01-07 15:31:48 +0100

    Multilayer perceptron

---
+++ Practicas/Practica6/MLP.py
@@ -0,0 +1,33 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+def MLP(x,y):
+        # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+    # Initialize MLPClassifier (you may need to adjust hyperparameters)
+    mlp_classifier = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=500, activation='relu', random_state=42)
+
+    # Train the model
+    mlp_classifier.fit(X_train, y_train)
+
+    # Make predictions on the test set
+    y_pred = mlp_classifier.predict(X_test)
+
+    # Evaluate the model
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+  
+
+    # Display confusion matrix
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Reds', cbar=False)
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+    # Display accuracy and MSE
+    print(f'Accuracy: {accuracy * 100:.2f}%')
